fd210bb9ea994f9e06e0ed06e12bdb45e9600f5b
apbelg <apbelg@mail.ru>
2017-03-08 23:39:37 +0300

    Add files via upload

---
+++ classes.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Настольная лампа
+ */
+class tableLamp
+{
+  public $color;
+  public $power;
+  public $typeLamp;
+  public $weight;
+  public $manufacturer;
+  private $shemaPower;
+
+  public function onLamp()
+  {
+    return $pressON;
+  }
+  public function offLamp()
+  {
+    return $pressOFF;
+  }
+  public function zamenaLampochki()
+  {
+    return $newLampochka;
+  }
+  public function MaxMinPower()
+  {
+    return $movingRegulator;
+  }
+  private function Remont()
+  {
+    $schemaOtLampy = $shemaPower;
+    return $adressRepairGoWithLamp;
+  }
+
+}
+
+/**
+ * Автомобильный навигатор
+ */
+class navigator
+{
+  public $razmerDisplay;
+  public $typeOS;
+  public $typeProgNavi;
+  public $kreplenie;
+  public $manufacturer;
+  private $baterry;
+  private $proshivka;
+
+  public function onNavi()
+  {
+    return $pressON;
+  }
+  public function offNavi()
+  {
+    return $pressOFF;
+  }
+  public function updateMapNavi()
+  {
+    return $newMapOrNewPO;
+  }
+  public function updateSoftwareNavi()
+  {
+    return $newSoftmovingRegulator;
+  }
+  private function updateProshivka()
+  {
+    return $goSpecialServiceForUpdateProshivka;
+  }
+  private function ReplaceBattery()
+  {
+    return $goSpecialServiceReplaceBattery;
+  }
+
+}
+
+
+ ?>
